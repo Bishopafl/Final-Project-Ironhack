78062e8ad208834fe2a3220a5e7f58af12423c65
Adam Lopez <Adamf04@gmail.com>
2015-12-05 19:45:57 -0500

    Have dr information showing. Search results bring back doctors that practice the user search

--- app/assets/javascripts/dashboards.js
+++ app/assets/javascripts/dashboards.js
@@ -1,2 +1,17 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+function searchApi(search){
+	var $search_bar = $(".js-search-bar").val();
+	console.log($search_bar)
+	var api_key = "66b0850367645bf27af70b06c3979f7f"
+	var resource_url = "https://api.betterdoctor.com/2014-09-12/doctors?query="+ $search_bar +"&skip=0&limit=10&user_key" + api_key;
+
+	$.ajax({
+		type: "GET",
+		url: resource_url,
+		success: function (responce){
+			console.log(searchApi, responce)
+		},
+		error: function (err){
+			console.log("Somethings not right.")
+		}
+	});
+}
--- app/assets/javascripts/specialists.js
+++ app/assets/javascripts/specialists.js
@@ -17,7 +17,7 @@ if ($('.container-fluid').hasClass('specialist-page')){
 	if ("geolocation" in navigator) {
 		console.log("geolocation is available!")
 
-		navigator.geolocation.getCurrentPosition(gotPosition, positionError);
+		navigator.geolocation.getCurrentPosition(onLocation, positionError);
 
 	} else {
 		console.log("geolocation is not available.")
@@ -32,16 +32,77 @@ function onLocation(position) {
 	};
 
 	createMap(myPosition);
+	searchApi(myPosition)
+	// add dr location to map 
 }
 
 function createMap(position) {
-	var = mapOptions = {
+	var mapOptions = {
 		center: position,
 		zoom: 17
 	};
 	map = new google.maps.Map($('#map')[0], mapOptions);
+	createMarker(position)
 }
 
 function onError(err) {
 	console.log("Your map isn't working...")
 }
+
+function createMarker(position) {
+	var marker = new google.maps.Marker({
+		position: position,
+		map: map
+	});
+}
+
+function searchApi (position) {
+	var latcoord = position.lat;
+	var lngcoord = position.lng;
+	var api_key = "66b0850367645bf27af70b06c3979f7f"
+	var resource_url = 'https://api.betterdoctor.com/2014-09-12/doctors?location='+latcoord+','+lngcoord+',100&skip=0&limit=10&user_key=' + api_key;
+
+	$.ajax({
+		url: resource_url,
+		success: function (response){
+			displayDoctors(response)
+		},
+		error: function(err){
+			console.log("not working bro");
+		}
+	});
+}
+
+
+function displayDoctors (response) {
+	console.log(response)
+	response.data.forEach(function(dr){
+		var name = dr.profile.first_name + " " + dr.profile.last_name;
+		var picture = dr.profile.image_url;
+		var phone = dr.practices[0].phones[0].number;
+		var street = dr.practices[0].visit_address.street;
+		var city = dr.practices[0].visit_address.city;
+		var state = dr.practices[0].visit_address.state;
+		var zip = dr.practices[0].visit_address.zip;
+		var html = '\
+			<li>\
+				<h3>'+ name +'</h3>\
+				<img class="image" src='+ picture +'>\
+				<br>\	
+				<span>'+ 'Contact: '+ phone +'</span>\
+				<div class="address">\
+					<span class="street">\
+					'+ street +'\
+					</span>\
+					<br>\
+					<span class="city-zip">\
+						'+city + ',' + state+ ',' + zip +'\
+					</span>\
+				</div>\
+			</li>';
+			
+		$(".js-dr-list").append(html);
+	});
+
+}
+
